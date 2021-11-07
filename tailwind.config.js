ad13916917dd28b75fb009fab7677ec8666aa656
Samuel Milla <samymbed@gmail.com>
2021-11-06 18:25:40 -0600

    custom colors y font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'becka-purple-darkest' : 'var(--becka-purple-darkest)',
+        'becka-neutral' : 'var(--becka-neutral)',
+        'becka-purple-dark':'var(--becka-purple-dark)',
+        'becka-purple-accent' : 'var(--becka-purple-accent)',
+        'becka-purple-light' : 'var(--becka-purple-light)',
+      },
+      fontFamily : {
+        'becka-sans' :  'Playfair Display, serif',
+      },
+    },
   },
   variants: {
     extend: {},
